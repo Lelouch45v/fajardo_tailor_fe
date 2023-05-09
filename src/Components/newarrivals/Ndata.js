22db90c42aa6da76b41d05172ede338015416991
david fajardo <lelouch45v@gmail.com>
2023-05-09 21:38:38 +0800

    new inserts photos and re calibrating

--- src/Components/newarrivals/Ndata.js
+++ src/Components/newarrivals/Ndata.js
@@ -1,33 +1,33 @@
 const Ndata = [
   {
-    cover: "./images/arrivals/arrivals1.png",
-    name: "Sunglass",
-    price: "150",
+    cover: "./images/arrivals/arrivals1.jpeg",
+    name: "Buko no Hero Academia Bedsheets",
+    price: "12",
   },
   {
-    cover: "./images/arrivals/arrivals2.png",
-    name: "Makeup",
-    price: "250",
+    cover: "./images/arrivals/arrivals2.jpg",
+    name: "League of legends Premium Bedsheets",
+    price: "30",
   },
   {
-    cover: "./images/arrivals/arrivals3.png",
-    name: "Smart Watch",
-    price: "50",
+    cover: "./images/arrivals/arrivals3.jfif",
+    name: "Tote Bag Cat design",
+    price: "2.50",
   },
   {
-    cover: "./images/arrivals/arrivals4.png",
-    name: "Lipstick",
-    price: "15",
+    cover: "./images/arrivals/arrivals4.jpeg",
+    name: "Zoro Limited Edition",
+    price: "20",
   },
   {
-    cover: "./images/arrivals/arrivals5.png",
-    name: "Green Plant",
-    price: "10",
+    cover: "./images/arrivals/arrivals5.jpg",
+    name: "Samurai Bedsheets",
+    price: "9.99",
   },
   {
-    cover: "./images/arrivals/arrivals6.png",
-    name: "Bonsai tree",
-    price: "400",
+    cover: "./images/arrivals/arrivals6.jpg",
+    name: "Black Union Company Design Tote bag",
+    price: "12",
   },
 ]
 
